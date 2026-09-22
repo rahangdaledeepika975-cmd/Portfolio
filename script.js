09b69c09673578a048da2a9e11ff5c88abb30600
Deepika Rahangdale <rahangdaledeepika975@gmail.com>
2026-09-22 17:25:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Smooth scrolling for navigation links
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener("click", function (event) {
+
+        const target = document.querySelector(this.getAttribute("href"));
+
+        if (target) {
+            event.preventDefault();
+
+            target.scrollIntoView({
+                behavior: "smooth"
+            });
+        }
+
+    });
+
+});
+
+
+// Console message
+
+console.log("Welcome to Deepika Rahangdale's Portfolio!");
